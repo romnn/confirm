2aab3673cfc6b4da458b5994800b5530d3b452dd
romnn <roman.dahm@gmx.de>
2024-10-21 22:13:18 +0200

    add cli arguments

--- main.go
+++ main.go
@@ -5,35 +5,90 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/spf13/cobra"
 )
 
-func confirm(message string, tries int) bool {
+func confirm(message string, count, maxAttempts int, verbose bool) bool {
 	r := bufio.NewReader(os.Stdin)
-	for tries > 0 {
-		tries--
-		fmt.Printf("%s [y/n]: ", message)
+	n := 0
+	for {
+		if n >= count {
+			// reached required confirm count
+			return true
+		}
+		attempt := 0
+		for {
+			if attempt >= maxAttempts {
+				// reached maximum number of attempts
+				return false
+			}
+			if verbose {
+				fmt.Printf(
+					"%s (%d/%d, attempt %d/%d) [y/n]: ",
+					message,
+					n+1,
+					count,
+					attempt+1,
+					maxAttempts,
+				)
+			} else {
+				fmt.Printf("%s [y/n]: ", message)
+			}
+			attempt++
 
-		res, err := r.ReadString('\n')
-		if err != nil {
-			panic(err)
+			res, err := r.ReadString('\n')
+			if err != nil {
+				panic(err)
+			}
+			// skip empty input (i.e. "\n")
+			if len(res) < 2 {
+				continue
+			}
+			confirmed := strings.ToLower(strings.TrimSpace(res))[0] == 'y'
+			if verbose {
+				fmt.Printf("confirmed=%v\n", confirmed)
+			}
+			if !confirmed {
+				return false
+			} else {
+				n++
+				break
+			}
 		}
-		// empty input (i.e. "\n")
-		if len(res) < 2 {
-			continue
-		}
-		return strings.ToLower(strings.TrimSpace(res))[0] == 'y'
 	}
-	return false
 }
 
 func main() {
 	message := "confirm"
-	if len(os.Args) > 1 {
-		message = os.Args[1]
+	tries := 1
+	count := 1
+	verbose := false
+	rootCmd := &cobra.Command{
+		Use:   "confirm",
+		Short: "confirm an action with yes/no",
+		Long:  "A dead simple CLI app that asks the user for a yes/no confirmation.",
+		Args:  cobra.MaximumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) > 0 {
+				message = args[0]
+			}
+			if confirm(message, count, tries, verbose) {
+				os.Exit(0)
+			} else {
+				os.Exit(1)
+			}
+		},
 	}
-	if confirm(message, 1) {
-		os.Exit(0)
-	} else {
+	rootCmd.PersistentFlags().
+		IntVarP(&tries, "tries", "t", 1, "maximum number of tries")
+	rootCmd.PersistentFlags().
+		IntVarP(&count, "count", "n", 1, "number of required confirmations")
+	rootCmd.PersistentFlags().
+		BoolVarP(&verbose, "verbose", "v", false, "enable verbose output")
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
+
 }
